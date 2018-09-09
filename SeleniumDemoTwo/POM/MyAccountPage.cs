a56e482ae09cf913f21ecae14eef370b9843d96d
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-08 20:35:57 -0400

    Added account page class

---
+++ SeleniumDemoTwo/POM/MyAccountPage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumDemoTwo.POM
+{
+    public class MyAccountPage
+    {
+        [Obsolete("Use newMethod instead", false)]
+
+        //initialize page object using constructor
+        public MyAccountPage()
+        {
+            PageFactory.InitElements(PropertiesCollection.Driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "Email")]
+        public IWebElement TxtEmail { get; set; }
+
+        [FindsBy(How = How.Id, Using = "Password")]
+        public IWebElement TxtPasswrd { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "login-button")]
+        public IWebElement BtnLogIn { get; set; }
+
+        //method to login
+
+    }
+}
