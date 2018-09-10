85c2c3318370d0c8f7ca214876777fb2470d0e87
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-09 21:29:40 -0400

    Build success after adding  Register class

--- SeleniumDemoTwo/POM/RegisteredSuccess.cs
+++ SeleniumDemoTwo/POM/RegisteredSuccess.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +10,19 @@ namespace SeleniumDemoTwo.POM
 {
     public class RegisteredSuccess
     {
+        [Obsolete("Use newMethod instead", false)]
         public RegisteredSuccess()
         {
-
+            PageFactory.InitElements(PropertiesCollection.Driver, this);
         }
+
+        [FindsBy(How = How.Name, Using = "register-continue")]
+        public IWebElement BtnContinue { get; set; }
 
         public StorePage Continue()
         {
-
+            BtnContinue.Click();
+            return new StorePage();
         }
     }
 }
--- SeleniumDemoTwo/TestCases/TestClasses.cs
+++ SeleniumDemoTwo/TestCases/TestClasses.cs
@@ -20,10 +20,10 @@ namespace SeleniumDemoTwo.TestCases
             PropertiesCollection.Driver.Navigate().GoToUrl("http://demo.nopcommerce.com");
 
             PropertiesCollection.Driver.Manage().Window.Maximize();
-
+            //click login on welcome page
             PropertiesCollection.Driver.FindElement(By.ClassName("ico-login"));
 
-            Console.WriteLine("Opened URL and clicked Login Page");
+            Console.WriteLine("Opened URL and clicked Welcome Page");
         }
 
         [Test]
@@ -35,6 +35,7 @@ namespace SeleniumDemoTwo.TestCases
             RegisterPage register = welcome.Register();
             MyAccountPage login = register.Register("Anu", "Sang", 11, "December", 2002, "asangeethu@yahoo.com","@nuK1978");
             Console.WriteLine("Registration done");
+            //click login for returning customer
             PropertiesCollection.Driver.FindElement(By.ClassName("ico-login"));
             System.Threading.Thread.Sleep(5000);
             welcome = new WelcomePage();
