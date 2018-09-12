477b43dfb11a2557182c2b7e713574d971937865
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-12 17:05:47 -0400

    Add Store page

--- SeleniumDemoTwo/POM/StorePage.cs
+++ SeleniumDemoTwo/POM/StorePage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +10,25 @@ namespace SeleniumDemoTwo.POM
 {
     public class StorePage
     {
+        [Obsolete("Use newMethod instead", false)]
+
+        //initialize page object using constructor
         public StorePage()
         {
-
+            PageFactory.InitElements(PropertiesCollection.Driver, this);
         }
 
+        [FindsBy(How = How.ClassName, Using = "product-item")]
+        IWebElement FeaturedProducts { get; set; }
 
+        [FindsBy(How = How.ClassName, Using = "add-to-cart-button")]
+        IWebElement AddCart { get; set; }
+
+        //shop item
+        public void ShopProduct()
+        {
+            AddCart.Click();
+        }
     }
 
 
