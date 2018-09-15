7c817bcf285ce80b2d771d389ec14c34c7cec116
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-15 00:29:33 -0400

    click on shopping cart

--- SeleniumDemoTwo/POM/StorePage.cs
+++ SeleniumDemoTwo/POM/StorePage.cs
@@ -23,7 +23,10 @@ namespace SeleniumDemoTwo.POM
         [FindsBy(How = How.XPath, Using = "/html/body/div[6]/div[3]/div/div/div/div/div[4]/div[2]/div[2]/div/div[2]/div[3]/div[2]/input[1]")]
         IWebElement AddCart { get; set; }
 
-        IWebElement AddCart2 { get; set; }
+        //IWebElement AddCart2 { get; set; }
+
+        [FindsBy(How = How.ClassName, Using = "cart-label")]
+        IWebElement ShoppingCart { get; set; }
 
 
             
@@ -33,7 +36,8 @@ namespace SeleniumDemoTwo.POM
             AddCart.Click();
             System.Threading.Thread.Sleep(5000);
             PropertiesCollection.Driver.FindElement(By.Id("add-to-cart-button-4")).Click();
-
+            ShoppingCart.Click();
+            Console.WriteLine("Click on shopping cart");
 
         }
     }
