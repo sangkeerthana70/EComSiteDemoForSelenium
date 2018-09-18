06a226bf534ca9c7da405927060654a55d2fc9ea
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-09-17 23:45:32 -0400

    Add properties for Final Checkout page

--- SeleniumDemoTwo/POM/FinalCheckout.cs
+++ SeleniumDemoTwo/POM/FinalCheckout.cs
@@ -18,21 +18,30 @@ namespace SeleniumDemoTwo.POM
             PageFactory.InitElements(PropertiesCollection.Driver, this);
         }
 
+        [FindsBy(How = How.Id, Using = "BillingNewAddress_FirstName")]
         IWebElement FirstName { get; set; }
 
+        [FindsBy(How = How.Id, Using = "BillingNewAddress_LastName")]
         IWebElement LastName { get; set; }
 
+        [FindsBy(How = How.Id, Using = "BillingNewAddress_Email")]
         IWebElement Email { get; set; }
 
+        [FindsBy(How = How.Id, Using = "BillingNewAddress_CountryId")]
         IWebElement Country { get; set; }
 
+        [FindsBy(How = How.Id, Using = "BillingNewAddress_City")]
         IWebElement City { get; set; }
 
+        [FindsBy(How = How.Id, Using = "BillingNewAddress_Address1")]
         IWebElement Address1 { get; set; }
 
+        [FindsBy(How = How.Id, Using = "BillingNewAddress_ZipPostalCode")]
         IWebElement Zip { get; set; }
 
+        [FindsBy(How = How.Id, Using = "BillingNewAddress_PhoneNumber")]
         IWebElement PhoneNumber { get; set; }
+
 
         IWebElement BtnContinue { get; set; }
     }
